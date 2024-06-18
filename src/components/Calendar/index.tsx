9ecd6b98b74ec21817039117851497a3b997b197
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-18 10:15:54 -0300

    Calendar visual structure

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,56 @@
+import { getWeekDays } from '@/utils/get-week-days'
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle></CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <CalendarDay>1</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>2</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>3</CalendarDay>
+          </td>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
