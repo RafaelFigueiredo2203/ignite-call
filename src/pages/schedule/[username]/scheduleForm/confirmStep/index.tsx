0673b9fa1acbfa152af01fefba7669835dfb5427
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-18 11:37:24 -0300

    Confirmation page

--- src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
+++ src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
@@ -1,3 +1,45 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
 export function ConfirmStep() {
-  return <div></div>
+  function handleScheduleConfirm() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleScheduleConfirm}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2024
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo:</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-email:</Text>
+        <TextInput type="email" placeholder="jhondoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações:</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
