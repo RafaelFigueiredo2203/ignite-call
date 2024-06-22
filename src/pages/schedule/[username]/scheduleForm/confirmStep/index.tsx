5e1616d9298ff3eb06f80fa4f6b84a8413ba2548
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-22 14:25:28 -0300

    Scheduling route

--- src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
+++ src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
@@ -1,6 +1,8 @@
+import { api } from '@/lib/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -33,8 +35,20 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleScheduleConfirm(data: ConfirmFormSchema) {
-    console.log(data)
+  const router = useRouter()
+  const username = String(router.query.username)
+
+  async function handleScheduleConfirm(data: ConfirmFormSchema) {
+    const { name, email, observations } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmConfirmation()
   }
 
   const describeDate = dayjs(schedulingDate).format('DDD[ de ]MMMM[ de ]YYYY')
